65ab99862f15b4823ac73f9f0b0240c78fa0e759
Lazar Pešić <lazar.pesic0109@gmail.com>
2019-01-03 23:42:50 +0100

    Bubble sort

---
+++ Data Structures and Algorithms/Sorting Algorithms/Bubble Sort.playground/Contents.swift
@@ -0,0 +1,31 @@
+import UIKit
+
+/*
+ The function always runs O(n^2) time even if the array is sorted. It can be optimized by stopping the algorithm if inner loop didn’t cause any swap.
+ */
+
+func main() {
+    var array: [Int] = [1, 3, 15, 6, 8, 12, 10, 33, 2, 88]
+//    var array: [Int] = [1, 2, 3, 6, 8, 10, 12, 15, 33, 88]
+    var swapped = false
+    
+    for _ in 0..<array.count {
+        for j in 0..<array.count - 1 {
+            if array[j] > array[j + 1] {
+//                var temp = array[j]
+//                array[j] = array[i]
+//                array[i] = temp
+                array.swapAt(j, j + 1)
+                swapped = true
+            }
+        }
+        
+        if swapped == false {
+            break
+        }
+    }
+    
+    print(array)
+}
+
+main()
