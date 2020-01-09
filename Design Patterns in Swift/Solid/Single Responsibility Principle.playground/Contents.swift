6e783130ae5aa1d724367f78cd812ac72608814f
Lazar Pešić <lazar.pesic0109@gmail.com>
2020-01-09 20:36:20 +0100

    design patterns in swift - solid (Single Responsibility Principle)

---
+++ Design Patterns in Swift/Solid/Single Responsibility Principle.playground/Contents.swift
@@ -0,0 +1,47 @@
+import Foundation
+import UIKit
+
+class Journal: CustomStringConvertible {
+    
+    var entries = [String]()
+    var count = 0
+    
+    func addEntry(_ text: String) -> Int {
+        count += 1
+        entries.append("\(count): \(text)")
+        return count - 1
+    }
+    
+    func removeEntry(_ index: Int) {
+        entries.remove(at: index)
+    }
+    
+    var description: String {
+        return entries.joined(separator: "\n")
+    }
+}
+
+class Persistence {
+    
+    func saveToFile(_ journal: Journal, _ filePathName: String, _ overvrite: Bool = false) {
+        
+    }
+    
+    func load(_ fileName: String) { }
+}
+
+func main() {
+    let j = Journal()
+    let _ = j.addEntry("I cried today")
+    let bug = j.addEntry("I ate a bug")
+    print(j)
+    j.removeEntry(bug)
+    print("---")
+    print(j)
+    
+    let p = Persistence()
+    let filename = "mnt/C/asdflakjsdf"
+    p.saveToFile(j, filename, false)
+}
+
+main()
