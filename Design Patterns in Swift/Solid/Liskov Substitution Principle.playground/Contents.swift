9d9c1f64e26155dcf9970fbe6a5d61cb777e68e2
Lazar Pešić <lazar.pesic0109@gmail.com>
2020-01-17 16:07:43 +0100

    design patterns in swift - solid (Liskov Substitution Principle)

---
+++ Design Patterns in Swift/Solid/Liskov Substitution Principle.playground/Contents.swift
@@ -0,0 +1,69 @@
+import UIKit
+
+///Example where is substitution principle is broken
+
+class Rectangle: CustomStringConvertible {
+    
+    fileprivate var _width = 0
+    fileprivate var _height = 0
+    
+    var width: Int {
+        get { return _width }
+        set { _width = newValue }
+    }
+    
+    var height: Int {
+        get { return _height }
+        set { _height = newValue }
+    }
+    
+    init(){}
+    
+    init(_ width: Int, _ height: Int) {
+        _width = width
+        _height = height
+    }
+    
+    var area: Int {
+        return width * height
+    }
+    
+    public var description: String {
+        return "Width: \(width), height: \(height)"
+    }
+}
+
+class Square: Rectangle {
+    
+    override var width: Int {
+        get { return _width }
+        set {
+            _width = newValue
+            _height = newValue
+        }
+    }
+    
+    override var height: Int {
+        get { return _width }
+        set {
+            _width = newValue
+            _height = newValue
+        }
+    }
+}
+
+func setAndMeasure(_ rc: Rectangle ) {
+    rc.width = 3
+    rc.height = 4
+    print("Expected area to be 12 but got \(rc.area)")
+}
+
+func main() {
+    let rc = Rectangle()
+    setAndMeasure(rc)
+    
+    let sq = Square()
+    setAndMeasure(sq)
+}
+
+main()
